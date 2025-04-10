91e324702c1f11b744fc317320462c368e147b1c
shahin-trunk <shahin.konadath@outlook.com>
2025-04-10 13:05:41 +0400

    First commit

--- yt_tools_test.py
+++ yt_tools_test.py
@@ -1,5 +1,6 @@
 from src import search_youtube, process_youtube
 
-urls = search_youtube("machupicchu", max_results=5)
+urls = search_youtube("home", max_results=5)
 
-res = process_youtube(urls[0], "res", "en")
+for url in urls:
+    res = process_youtube(url, "res", "en")
