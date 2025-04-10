1e9c47ca4367cc5381bf50ada91e4cbc9e351d30
shahin-trunk <shahin.konadath@outlook.com>
2025-04-10 12:55:45 +0400

    First commit

---
+++ src/tools/__init__.py
@@ -0,0 +1,3 @@
+from src.tools.yt_tools import search_youtube, process_youtube
+
+__all__ = ["search_youtube", "process_youtube"]
--- test/yt_tools_test.py
+++ test/yt_tools_test.py
@@ -1,4 +1,4 @@
-from yt_tools import search_youtube, process_youtube
+from src.tools import search_youtube, process_youtube
 
 urls = search_youtube("machupicchu", max_results=5)
 
